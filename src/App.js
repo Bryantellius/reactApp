d5d70b27c68b5bf77d9d54708e766ad91abe3b1c
Matt Brogan <broganmatt11@gmail.com>
2020-08-07 18:19:03 -0400

    style update

--- src/App.js
+++ src/App.js
@@ -42,14 +42,14 @@ class App extends React.Component {
         <Container className="headbar" fluid={true}>
           
           <Navbar className="border-bottom" bg="transparent" expand="lg">
-            <Navbar.Brand className="navbrand">Matthew Brogan</Navbar.Brand>
+            <Navbar.Brand style={{color: 'white'}}>Matthew Brogan</Navbar.Brand>
 
             <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
             <Navbar.Collapse id="navbar-toggle">
               <Nav className="ml-auto">
-                <Link className="nav-link" to="/">Home</Link>
-                <Link className="nav-link" to="/about">About</Link>
-                <Link className="nav-link" to="/contact">Contact</Link>
+                <Link className="nav-link" to="/" style={{color: 'white'}}>Home</Link>
+                <Link className="nav-link" to="/about" style={{color: 'white'}}>About</Link>
+                <Link className="nav-link" to="/contact" style={{color: 'white'}}>Contact</Link>
               </Nav>
             </Navbar.Collapse>
           </Navbar>
